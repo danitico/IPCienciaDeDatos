b6b8ca8aa3940e337d09cc81b7b5208346443372
Daniel Ranchal <danielranchal@correo.ugr.es>
2021-10-04 23:38:02 +0200

    Finished strings exercise

--- r/part4/strings/ejercicio5.R
+++ r/part4/strings/ejercicio5.R
@@ -3,3 +3,12 @@
 # y año crea un vector con las fechas completas. Si la fecha es inválida, ésta se descartará
 # (Ayuda: investiga la función as.Date).
 
+days <- c(2, 32, 28)
+months <- c(11, 45, 4)
+years <- c(1998, 2002, 1999)
+
+datetimes <- paste(days, months, years, sep='-')
+
+datetimes[
+  which(tryCatch(as.Date(datetimes), error = function(err) {FALSE}) != FALSE)
+]
