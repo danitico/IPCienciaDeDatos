4bc870556f8afb9862d295dba9f6b49e6ad9375f
Daniel Ranchal <danielranchal@correo.ugr.es>
2021-10-06 20:42:13 +0200

    Exercise 1 and 2 done

---
+++ r/part5/ejercicio2.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+
+ex2 <- data.frame(year=c(1970:1979), snow.cover=c(6.5,12.0,14.9,10.0,10.7,7.9,21.9,12.5,14.5,9.2))
+
+ggplot(
+  ex2,
+  aes(
+    x=as.character(year),
+    y=snow.cover
+  )
+) + geom_bar(fill="blue", stat="identity", width = .5) + labs(title="Snow every year", x="Year", y="Snow cover")
+
+ggplot(
+  ex2,
+  aes(x=snow.cover)
+) + geom_histogram(bins = 4) + labs(title="Histogram of snow cover", x="Snow cover")
